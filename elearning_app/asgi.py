1a123abc47f24e1337109efdaa323c435b09584c
Deston <destontan0@gmail.com>
2026-03-08 18:12:45 +0800

    add redis to docker and fix websocket routing

--- elearning_app/asgi.py
+++ elearning_app/asgi.py
@@ -7,17 +7,35 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning_app.settings')
+
+# # The traffic cop
+# application = ProtocolTypeRouter({
+#     # 1. Standard HTTP requests are routed to normal Django views
+#     "http": get_asgi_application(),
+    
+#     # 2. WebSocket traffic will be routed here later!
+#     # "websocket": ...
+# })
+
+
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import elearning_app.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning_app.settings')
 
-# The traffic cop
 application = ProtocolTypeRouter({
-    # 1. Standard HTTP requests are routed to normal Django views
     "http": get_asgi_application(),
-    
-    # 2. WebSocket traffic will be routed here later!
-    # "websocket": ...
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            elearning_app.routing.websocket_urlpatterns
+        )
+    ),
 })
--- elearning_app/routing.py
+++ elearning_app/routing.py
@@ -5,16 +5,29 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elearning_app.settings')
 django_asgi_app = get_asgi_application()
 
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# import chat.routing
+
+# application = ProtocolTypeRouter({
+#     #standard HTTP requests (normal web pages) are route here
+#     "http": django_asgi_app,
+
+#     #websocket traffic (the real-time chat) are route here
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
 application = ProtocolTypeRouter({
-    #standard HTTP requests (normal web pages) are route here
-    "http": django_asgi_app,
-
-    #websocket traffic (the real-time chat) are route here
-    "websocket": AuthMiddlewareStack(
+    #(http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
